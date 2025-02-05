bd1743f0200ad1c386b6937059c567d34e6f0420
Duncan Everson <duncan.everson@icloud.com>
2025-02-05 01:18:50 -0800

    created initial javascript react pages for the 4 main pages of the website.

---
+++ code/client/src/DirHome/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        Home Page
+      </header>
+      <p>Hello from Home page</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ code/client/src/DirNutrition/Nutrition.js
@@ -0,0 +1,12 @@
+function Nutrition() {
+  return (
+    <div className="Nutrition">
+      <header className="Nutrition-header">
+        Nutrition Page
+      </header>
+      <p>Hello from Nutrition page</p>
+    </div>
+  );
+}
+
+export default Nutrition;
