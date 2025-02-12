f7f8de60ab9267efb1a1597c683cf3ec06867455
Duncan Everson <duncan.everson@icloud.com>
2025-02-11 19:00:10 -0800

    adjusted the app.js code with routes so that pages are served depending on the url path that the imported navbar makes.

--- code/client/src/DirApp/App.js
+++ code/client/src/DirApp/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../components/DirNavbar/Navbar";
+import Home from "../pages/DirHome/Home";
+import Food from "../pages/DirFood/Food";
+import Grocery from "../pages/DirGrocery/Grocery";
+import Nutrition from "../pages/DirNutrition/Nutrition";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />  {/* Visible on all pages */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/food" element={<Food />} />
+        <Route path="/grocery" element={<Grocery />} />
+        <Route path="/nutrition" element={<Nutrition />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
