079e112c4f05261654ea7f4800eb326a77f0e967
Duncan Everson <duncan.everson@icloud.com>
2025-02-11 19:02:40 -0800

    moved the react page's directories into the pages directory to house all of the page content.

---
+++ code/client/src/pages/DirFood/Food.js
@@ -0,0 +1,12 @@
+function Food() {
+  return (
+    <div className="Food">
+      <header className="Food-header">
+        Food Page
+      </header>
+      <p>Hello from Food page</p>
+    </div>
+  );
+}
+
+export default Food;
