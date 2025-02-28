3197b4c52d96e5e1386d5952812c46582630b472
shiningn-osu <shiningn@oregonstate.edu>
2025-02-28 11:12:24 -0800

    change port to 5000

--- code/server/server.js
+++ code/server/server.js
@@ -108,5 +108,5 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
